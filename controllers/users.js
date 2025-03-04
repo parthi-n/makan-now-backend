8d71ef1efc63a665449abb4cfe7098cedfc08764
parthi-n <parthiban.n@live.com>
2025-03-05 02:39:19 +0800

    updated ordering

--- controllers/users.js
+++ controllers/users.js
@@ -33,17 +33,25 @@ const addFoodOrders = async (req, res) => {
 		if (req.user._id !== req.params.userId) {
 			return res.status(403).json({ err: "Unauthorized" });
 		}
+
 		const user = await User.findById(req.params.userId);
 		if (!user) {
 			return res.status(404).json({ err: "User not found." });
 		}
 
-		user.orders.push(...req.body);
-		await user.save();
-
-		res.status(201).json(user);
+		if (Array.isArray(req.body) && req.body.length > 0) {
+			user.ordersList.push(...req.body);
+			await user.save();
+			res.status(201).json(user.ordersList.slice(-req.body.length));
+		} else if (req.body && typeof req.body === "object") {
+			user.ordersList.push(req.body);
+			await user.save();
+			res.status(201).json(req.body);
+		} else {
+			res.status(400).json({ err: "Request body must be an object or an array of menu items" });
+		}
 	} catch (error) {
-		res.status(500).json({ err: err.message });
+		res.status(500).json({ err: error.message });
 	}
 };
 
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const verifyToken = require("../middleware/verify-token");
 const usersCtrl = require("../controllers/users");
 
 router.get("/", verifyToken, usersCtrl.index);
-router.get("/:userId", usersCtrl.get);
-router.put("/:userId", usersCtrl.addFoodOrders);
+router.get("/:userId",verifyToken, usersCtrl.get);
+router.put("/:userId/orders",verifyToken, usersCtrl.addFoodOrders);
 
 module.exports = router;
